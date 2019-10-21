c55f40c8eb303d438acc2327468f6352d65f0755
zhaosaisai <zhaosaisai@u51.com>
2019-10-21 21:27:13 +0800

    normal: 添加template to interface.js

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+import ejs from 'ejs'
 import { getSwaggerJson } from './lib/service'
 import  makeParamsObject from './lib/makeParamsObject'
 import convertDefinition2Interface from './lib/convertDefinition2Interface'
 
 getSwaggerJson('http://stable.treasureboxgateway.51.env/v2/api-docs?group=TreasureBoxGateway').then(json => {
-	console.log(JSON.stringify(convertDefinition2Interface(json.definitions), null, 2))
+	const d = convertDefinition2Interface(json.definitions)
+	console.log(JSON.stringify(d, null, 2))
+
+	ejs.renderFile('./lib/templates/interface.ejs', {
+		interfaces: d
+	}).then(data => {
+		console.log(data)
+	})
 })
